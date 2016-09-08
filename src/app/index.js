2e928775a94d49d542bf6f3d89605b785a5fcea8
KLC215 <kwanlamc05@gmail.com>
2016-09-08 12:09:01 +0800

    Update entry point

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
-console.log('Hello React!');
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ *  Entry Component
+ */
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello React!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
-var webpack = require('webpack');
-var path    = require('path');
+var webpack            = require('webpack');
+var path               = require('path');
 
-var DIST_DIR = path.resolve(__dirname, 'dist');
-var SRC_DIR  = path.resolve(__dirname, 'src');
+var DIST_DIR  = path.resolve(__dirname, 'dist');
+var SRC_DIR   = path.resolve(__dirname, 'src');
+
 
 var config = {
-    entry : SRC_DIR + '/app/index.js',
+
+    entry: SRC_DIR + '/app/index.js',
+
     output: {
         path      : DIST_DIR + '/app',
         filename  : 'bundle.js',
         publicPath: '/app/'
     },
+
     module: {
         loaders: [
             {
@@ -20,9 +24,10 @@ var config = {
                 query  : {
                     presets: ["react", "es2015", "stage-2"]
                 }
-            }
+            },
         ]
     }
+
 };
 
 module.exports = config;
